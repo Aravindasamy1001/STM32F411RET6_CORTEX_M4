7b7f9f63f36d9fedf744bc054a74fde02ccef27e
Aravindasamy v <aravindasamy.v.1001@gmail.com>
2026-06-03 16:15:37 +0530

    Update USER_LED_STM32.c

--- PROTOCOLS/USER_LED_STM32.c
+++ PROTOCOLS/USER_LED_STM32.c
@@ -1,1 +1,29 @@
+#include <stdint.h>
 
+#define RCC_AHB1ENR (*(volatile uint32_t*)0x40023830)
+#define GPIOA_MODER (*(volatile uint32_t*)0x40020000)
+#define GPIOA_ODR (*(volatile uint32_t*)0x40020014)
+
+
+void delay(){
+	int i;
+	for(i=0;i<1000000;i++){
+	}
+	}
+
+int main(void)
+{
+
+	RCC_AHB1ENR |=(1<<0);
+	GPIOA_MODER &=~(3<<10);
+	GPIOA_MODER |=(1<<10);
+    /* Loop forever */
+	while(1){
+
+		GPIOA_ODR |=(1<<5);
+		delay();
+		GPIOA_ODR =(0<<5);
+		delay();
+
+	}
+}
